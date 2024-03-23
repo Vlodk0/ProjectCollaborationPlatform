875055a9ee728a9f3f3519f1f3c4e36f404daee2
Vladislav Kyi <v.kyy@nltu.lviv.ua>
2024-03-23 16:52:54 +0200

    created logic of adding technologies for developers

--- ProjectCollaborationPlatform.BL/Interfaces/ITechnologyService.cs
+++ ProjectCollaborationPlatform.BL/Interfaces/ITechnologyService.cs
@@ -7,7 +7,9 @@ namespace ProjectCollaborationPlatform.BL.Interfaces
     public interface ITechnologyService
     {
         public Task<bool> AddTechnologyForProject(Guid Id, List<ProjectTechnologyIdDTO> projectTechnologyIdDTO);
-        public Task<bool> RemoveTechnologyForProject(Guid Id, List<ProjectTechnologyIdDTO?> projectTechnologyIdDTO);
+        public Task<bool> AddTechnologyForDeveloper(Guid Id, List<DeveloperTechnologyIdDTO> developerTechnologyIdDTO);
+        public Task<bool> RemoveTechnologyFromProject(Guid Id, List<ProjectTechnologyIdDTO?> projectTechnologyIdDTO);
+        public Task<bool> RemoveTechnologyFromDeveloper(Guid Id, List<DeveloperTechnologyIdDTO?> developerTechnologyIdDTO);
         public Task<bool> SaveTechnologiesAsync();
     }
 }
